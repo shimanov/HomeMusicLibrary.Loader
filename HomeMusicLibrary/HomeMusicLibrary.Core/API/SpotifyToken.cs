138e8938394784ce028be09592aa696e1c3eaa78
Dmitriy <d.a.shimanov@gmail.com>
2022-06-07 23:11:29 +0700

    Реализовал класс получения токена

--- HomeMusicLibrary/HomeMusicLibrary.Core/API/SpotifyToken.cs
+++ HomeMusicLibrary/HomeMusicLibrary.Core/API/SpotifyToken.cs
@@ -3,12 +3,12 @@ using SpotifyAPI.Web;
 
 public class SpotifyToken
 {
-    static Task<string> Token()
+    public async Task Token()
     {
-        var config = SpotifyClientConfig
-            .CreateDefault()
-            .WithAuthenticator(new ClientCredentialsAuthenticator("23764d882c704915ae47b1f94fa5714e", "23764d882c704915ae47b1f94fa5714e"));
-        var spotify = new SpotifyClient(config);
-        return Task.FromResult(spotify.ToString());
+        var config = SpotifyClientConfig.CreateDefault();
+        var request =
+            new ClientCredentialsRequest("23764d882c704915ae47b1f94fa5714e", "d39b85cda0ee461b8cc84f2e7984f907");
+        var response = await new OAuthClient(config).RequestToken(request);
+        var spotiToken = new SpotifyClient(config.WithToken(response.AccessToken));
     }
 }
--- HomeMusicLibrary/HomeMusicLibrary.Core/Program.cs
+++ HomeMusicLibrary/HomeMusicLibrary.Core/Program.cs
@@ -2,9 +2,8 @@
 
 using HomeMusicLibrary.Core.API;
 
-
-
-
+var spToken = new SpotifyToken();
+await spToken.Token();
 
 const string token = "BQD5dxS1IT-otQVylbBo8LQNH8BdGjOwhieaGYeHx2MZ41IPg5MUepoEst3DJA4CJuZXcaVCqOWWEhdWB4guy_tq0D8OWAp2wS3lASCdvwfmOSgKL1vg_15huJCJSrD6DFaGsIzuxwdxO0ASxn1L3lB2kdgDTLE";
 
