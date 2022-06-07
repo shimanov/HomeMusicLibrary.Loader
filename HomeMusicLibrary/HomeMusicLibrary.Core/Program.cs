99f0d4c0b06b8882f22973719555efdc20866540
Dmitriy <d.a.shimanov@gmail.com>
2022-06-07 22:17:17 +0700

    Изменил метод Main

--- HomeMusicLibrary/HomeMusicLibrary.Core/Program.cs
+++ HomeMusicLibrary/HomeMusicLibrary.Core/Program.cs
@@ -1,18 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using HomeMusicLibrary.Core.API;
-using SpotifyAPI.Web;
+
+var _token = "BQD5dxS1IT-otQVylbBo8LQNH8BdGjOwhieaGYeHx2MZ41IPg5MUepoEst3DJA4CJuZXcaVCqOWWEhdWB4guy_tq0D8OWAp2wS3lASCdvwfmOSgKL1vg_15huJCJSrD6DFaGsIzuxwdxO0ASxn1L3lB2kdgDTLE";
 
 var search = new Search
 {
-    token = "BQCa0HcJkNdLu6zLySpGieAas9cr534rCJtUEc1sW3pxkQ88MdFJ3k75MDQ2d8WR0RfvEq7vEodpDfJJ_M4pHwrZhmbDivjzXz8RA4dwcoS9KMaKsF0daC9z-CjlGpOuGoQIhayyg27JxhJG4gPNHXoLq9MAiy4",
+    token = _token,
     artist = "Rammstein"
 };
 await search.Artist();
 
 var album = new AlbumInfo()
 {
-    token = "BQCa0HcJkNdLu6zLySpGieAas9cr534rCJtUEc1sW3pxkQ88MdFJ3k75MDQ2d8WR0RfvEq7vEodpDfJJ_M4pHwrZhmbDivjzXz8RA4dwcoS9KMaKsF0daC9z-CjlGpOuGoQIhayyg27JxhJG4gPNHXoLq9MAiy4",
+    token = _token,
     artistId = "0lVlNsuGaOr9vMHCZIAKMt"
 };
 await album.Album();
