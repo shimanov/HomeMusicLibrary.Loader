2328a3e46efa5bbf5bd86bad62d507bcd060c3d2
Dmitriy <d.a.shimanov@gmail.com>
2022-06-07 21:43:15 +0700

    Добавил класс который выводит информацию по альбому

---
+++ HomeMusicLibrary/HomeMusicLibrary.Core/API/AlbumInfo.cs
@@ -0,0 +1,23 @@
+using SpotifyAPI.Web;
+
+namespace HomeMusicLibrary.Core.API;
+
+public class AlbumInfo
+{
+    public string token;
+    public string albumId;
+
+    public async Task Album()
+    {
+        var spotifyAblum = new SpotifyClient(token);
+        var searchAlbums = await spotifyAblum.Artists.GetAlbums(albumId);
+        if (searchAlbums.Items != null)
+        {
+            foreach (var album in searchAlbums.Items)
+            {
+                Console.WriteLine("Album: {0}\n realise date: {1}\n Type: {2}\n Total Tracks: {3}\n Id: {4}\n\n",
+                                 album.Name, album.ReleaseDate, album.Type, album.TotalTracks, album.Id);
+            }
+        }
+    }
+}
