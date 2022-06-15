46e12ce8279a6f8912adfa02bc779433df774a74
Dmitriy <d.a.shimanov@gmail.com>
2022-06-15 22:03:30 +0700

    Изменил метод сравнения названий

--- HomeMusicLibrary/HomeMusicLibrary.Core/API/Search.cs
+++ HomeMusicLibrary/HomeMusicLibrary.Core/API/Search.cs
@@ -12,20 +12,26 @@ public class Search
         var search = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.Artist, artist));
         await foreach (var a in spotify.Paginate(search.Artists, (s) => s.Artists))
         {
-            if (a.Name != artist) continue;
-            await using (var context = new ApplicationContext())
+            if (artist.Equals(a.Name))
             {
-                var artist = new Model.Artist
+                await using (var context = new ApplicationContext())
                 {
-                    Id = Guid.NewGuid().ToString(),
-                    ArtistName = a.Name,
-                    ArtistId = a.Id
-                };
-                await context.Artists.AddRangeAsync(artist);
-                await context.SaveChangesAsync();
+                    var artist = new Model.Artist
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        ArtistName = a.Name,
+                        ArtistId = a.Id,
+                    };
+                    await context.Artists.AddRangeAsync(artist);
+                    await context.SaveChangesAsync();
+                }
+                Console.WriteLine("Artist: {0}\n ID: {1}\n", a.Name, a.Id);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
             }
             
-            Console.WriteLine("Artist: {0}\n ID: {1}\n", a.Name, a.Id);
         }
     }
 }
--- HomeMusicLibrary/HomeMusicLibrary.Core/Program.cs
+++ HomeMusicLibrary/HomeMusicLibrary.Core/Program.cs
@@ -9,24 +9,30 @@ string token = await spToken.Token();
 //Шаг 2. Берем из БД ID артиста и находим все альбомы
 //Шаг 3. Берем ID альбома и ищем его
 
-
-var search = new Search
+string[] art = File.ReadAllLines(Environment.CurrentDirectory + @"/art.txt");
+foreach (string s in art)
 {
-    token = token,
-    artist = "MajorVoice"
-};
-await search.Artist();
+    string name = s;
+    var searchArt = new Search()
+    {
+        token = token,
+        artist = name
+    };
+    await searchArt.Artist();
+}
 
-var album = new AlbumInfo()
-{
-    token = token,
-    artistId = "6oBT7BaxDA9UakaNepYMCY"
-};
-await album.Album();
 
-var tracks = new AlbumTracks()
-{
-    token = token,
-    albumsId = "3h8uLEWJtLmfVXtMEMkvPu"
-};
-await tracks.Tracks();
+
+// var album = new AlbumInfo()
+// {
+//     token = token,
+//     artistId = "6oBT7BaxDA9UakaNepYMCY"
+// };
+// await album.Album();
+//
+// var tracks = new AlbumTracks()
+// {
+//     token = token,
+//     albumsId = "3h8uLEWJtLmfVXtMEMkvPu"
+// };
+// await tracks.Tracks();
